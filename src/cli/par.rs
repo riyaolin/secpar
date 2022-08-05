5e4259c25ace5ac90c5c1d355915712fcbb4eb00
Riyao Lin <linriyao@gmail.com>
2022-08-05 11:37:31 -0400

    added get_parameter command

--- src/cli/par.rs
+++ src/cli/par.rs
@@ -1,12 +1,13 @@
 use crate::errors::SecParError;
 use crate::opt::ParCommand;
 use aws_sdk_ssm::{Client, Region};
+use color_eyre::eyre::eyre;
 use color_eyre::Report;
 use tokio_stream::StreamExt;
 use tracing::{debug, info};
 
 /// list all parameters
-pub async fn list_parameters(client: &Client) -> Result<(), SecParError> {
+async fn list_parameters(client: &Client) -> Result<(), SecParError> {
     let mut pars_stream = client.describe_parameters().into_paginator().send();
     let mut p_count = 1;
     while let Some(par_output) = pars_stream.next().await {
@@ -24,6 +25,23 @@ pub async fn list_parameters(client: &Client) -> Result<(), SecParError> {
     Ok(())
 }
 
+/// get speficied parameter
+async fn get_parameter(client: &Client, name: &str) -> Result<String, SecParError> {
+    match client
+        .get_parameter()
+        .name(name)
+        .with_decryption(true)
+        .send()
+        .await
+    {
+        Ok(output) => Ok(output.parameter().unwrap().value.clone().unwrap()),
+        Err(e) => {
+            debug!("Error: {:?}", e.to_string());
+            Err(SecParError::NotFound(e.to_string()))
+        }
+    }
+}
+
 pub async fn process_par_command(command: &ParCommand) -> Result<(), Report> {
     let region_provider = Region::new("us-east-1");
     let shared_config = aws_config::from_env().region(region_provider).load().await;
@@ -32,9 +50,15 @@ pub async fn process_par_command(command: &ParCommand) -> Result<(), Report> {
         ParCommand::List {} => {
             list_parameters(&client).await?;
         }
-        ParCommand::Get { name: _ } => {
-            todo!();
-        }
+        ParCommand::Get { name } => match get_parameter(&client, &name).await {
+            Ok(par_value) => {
+                info!("Got parameter[{}] value:", name);
+                info!("  {}", par_value);
+            }
+            Err(_) => {
+                return Err(eyre!("Failed to retrieve parameter value"));
+            }
+        },
     }
     Ok(())
 }
